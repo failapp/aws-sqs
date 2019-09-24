a8ceb706180e70a1e8e1d47bb825b84f88cad225
failapp <jrodriguez@aol.cl>
2019-09-24 14:02:22 -0300

    first commit

---
+++ src/test/java/com/inutilapp/sqs/SqsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.inutilapp.sqs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SqsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
